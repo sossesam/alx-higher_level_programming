0b09559c15d31e439fe8967e95284b3deac88260
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-29 16:25:59 +0100

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(100):
     if i < 99:
-        print("{}{}".format((int(i / 10)), (int(i % 10))), end = ", ")
+        print("{}{}".format((int(i / 10)), (int(i % 10))), end=", ")
     else:
-        print("{}{}".format((int(i / 10)), (int(i % 10))), end = "\n")
+        print("{}{}".format((int(i / 10)), (int(i % 10))), end="\n")
