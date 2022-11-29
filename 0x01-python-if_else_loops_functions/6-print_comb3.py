61b46f915f110c0a134da008f8f30bd081c6d8bc
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-29 16:47:01 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,4 @@ for i in range(1, 90):
         if (i / 10) <= (i % 10):
             print("{}{}".format((int(i / 10)), (int(i % 10))), end=", ")
     else:
-         print("{}{}".format((int(i / 10)), (int(i % 10))), end="\n")
-
-    
+        print("{}{}".format((int(i / 10)), (int(i % 10))), end="\n")
