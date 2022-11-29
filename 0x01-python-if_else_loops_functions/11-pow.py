7d956e22c059b2518b178383da13b29ad91cacde
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-30 00:14:20 +0100

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    length = b
-    if b < 0:
-        length = length * -1
-
-    for i in range(length):
-        if b <= 0:
-            result = result / a
-        else:
-            result = result * a
-
-    return float(result)
+    return a ** b
+    
