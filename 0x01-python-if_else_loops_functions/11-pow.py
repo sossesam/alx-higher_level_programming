e032b5686d7c6716713c6ae5ddda6277d1bd657f
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-29 22:57:53 +0100

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,15 +2,14 @@
 
 def pow(a, b):
     result = 1
-    i = 1
     length = b
     if b < 0:
         length = length * -1
 
     for i in range(length):
-        if b < 0:
+        if b <= 0:
             result = result / a
         else:
             result = result * a
 
-    return result
+    return float(result)
