31b0532e3ce670c59be63235272058482997b3ee
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-29 21:38:31 +0100

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    result = 1
+    i = 1
+    length = b
+    if b < 0:
+        length = length * -1
+
+    for i in range(length):
+        if b <= 0:
+            result = result / a
+        else:
+            result = result * a
+        
+    return result
