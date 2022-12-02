5ea7505c69136a087e2b3beb4b04c3568697e90a
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-02 07:54:56 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,7 @@ x = (sys.argv)
 if n > 0:
     print(f"{n} arguments:")
     for i in range(n):
-        print(f"{i + 1}: {x[i + 1]}")      
+        print(f"{i + 1}: {x[i + 1]}")
 else:
     print(f"{n} arguments.")
+    
