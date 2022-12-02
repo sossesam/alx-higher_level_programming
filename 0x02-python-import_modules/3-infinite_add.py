32039ae00e5bacc2aafef0a42b01abbf4230938c
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-02 08:55:47 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
 
 x = argv[1:]
 result = 0
-for i in range(len(argv) - 1):
-    result = result + int(x[i])
+for i in argv[1:]:
+    result = result + int(i)
+
+print(f"{result}")
