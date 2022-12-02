000b348bb9ce5711c5e0153c120f9521e8fa878b
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-02 08:17:26 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
+    from sys import argv
 
-n = len(sys.argv) - 1
-x = (sys.argv)
+n = len(argv) - 1
 if n > 0:
     print(f"{n} arguments:")
     for i in range(n):
-        print(f"{i + 1}: {x[i + 1]}")      
+        print(f"{i + 1}: {argv[i + 1]}")      
 else:
     print(f"{n} arguments.")
