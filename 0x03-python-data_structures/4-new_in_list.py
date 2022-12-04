ae5eb105827655429aebcb09aefd732783d93425
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-04 21:43:18 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    new_List= my_list[:]
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+       new_List[idx] = element
+       return new_List
