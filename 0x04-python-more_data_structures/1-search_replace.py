310b17a1f3b26f0503ec40f69b9594be63ad8fae
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-07 15:58:02 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-        newNumber = lambda x :  replace if(x == search) else x
-        return list(map(newNumber, my_list))
+    newNumber = lambda x:  replace if (x == search) else x
+    return list(map(newNumber, my_list))
