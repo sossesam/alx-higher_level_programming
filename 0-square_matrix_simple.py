7d6e36a43def208d45abfe1b158acd3713d1029c
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-07 15:01:26 +0100

    0-square_matrix_simple.py

---
+++ 0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    newMatrix = []
+    for first in matrix:
+        secondMatrix = []
+        newMatrix.append(secondMatrix)
+
+        for second in first:
+            newValue = second * second
+            secondMatrix.append(newValue)
+
+    return newMatrix
