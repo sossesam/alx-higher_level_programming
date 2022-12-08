795eff7eb594446f24b8e657cadbd728d99d5337
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-08 07:48:04 +0100

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,6 @@ def weight_average(my_list=[]):
     def mul(x):
         return x[0] * x[1]
 
-
     if len(my_list) > 0:
         for x in my_list:
          result =  round(sum(list(map(mul,my_list))), 2)/round(sum(list(map(lambda x: x[1], my_list))), 2)
