49ec3591e7f20625b6893f8032d0943cacdc24ce
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-08 19:15:30 +0100

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_list= list(roman_string)
+    roman_list = list(roman_string)
     roman_length = len(roman_list)
-    roman_dict = {'M' : 1000, 'D' : 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 }
+    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'v': 10,
+                  'V': 5, 'I': 1}
     result = 0
     count = 0
     while count < roman_length:
-            if roman_list[count] not in roman_dict.keys():
-                result = None
-                break
-            else:
-                if (roman_dict[roman_list[count]] > roman_dict[roman_list[count - 1]] ) and (count == 1):
-                    result = roman_dict[roman_list[count]] - (result)
-                    count += 1
-                elif (roman_dict[roman_list[count]] > roman_dict[roman_list[count - 1]] ):
-                    result = roman_dict[roman_list[count]] - (result * 2)
-                    count += 1
-                else:
-                    result += roman_dict[roman_list[count]]
-                    count += 1
-                
-    
-    return result
+        if roman_list[count] not in roman_dict.keys():
+            result = None
+            break
+        else:
+            v = roman_dict[roman_list[count]]
+            x = x
 
+            if (v > x) and (count == 1):
+                result = v - (result)
+                count += 1
+            elif (v > x):
+                result = v - (result * 2)
+                count += 1
+            else:
+                result += v
+                count += 1
+    return result
