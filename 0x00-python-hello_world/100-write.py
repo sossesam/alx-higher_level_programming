43a7b4eb1b619bb8500f115d1bf105d12e5388b5
oyeniran samuel sanmi <sossesam@gmail.com>
2023-06-15 16:27:53 +0100

    all

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
-import sys
 
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-exit(1)
+import dis
+
+def magic_calculation(a, b):
+    return 98 + (a ** b)
+
+
+dis.dis(magic_calculation)
