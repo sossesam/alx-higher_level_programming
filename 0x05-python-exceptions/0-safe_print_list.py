ea22fc12f31df14ac2cef4f95068897d9515a562
oyeniran samuel sanmi <sossesam@gmail.com>
2023-06-29 08:25:20 +0100

    all

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,15 +3,13 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     z = 0
     for y in my_list:
-        i+=1
+        i += 1
     while z < i and z < x:
         try:
             print(my_list[z], end="")
-            z+=1
-        except:
+            z += 1
+        except Exception:
             ""
             return z
-    print("",end="\n")
+    print("", end = "\n")
     return z
-
-
