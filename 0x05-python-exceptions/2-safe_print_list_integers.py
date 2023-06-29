237907952a06f7defc33b84093303cef6364ecb0
oyeniran samuel sanmi <sossesam@gmail.com>
2023-06-29 15:13:00 +0100

    all

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 def is_int(x):
 
-        try:
-            if x + 1:
-                return x
-        except:
-             return
+    try:
+        if x + 1:
+            return x
+    except Exception:
+        return
 
-def safe_print_list_integers(my_list=[], x=0):
+
+def safe_print_list_integers(my_list = [], x = 0):
     i = 0
     check = 0
     for y in my_list:
         try:
             if y + 1:
-                i+=1
-        except:
-                  i
+                i += 1
+        except Exception:
+            i
 
     y = 0
     for z in range(x):
         try:
             if is_int(my_list[z]):
-                print("{:d}".format(my_list[z]), end="")
-                y+=1
+                print("{:d}".format(my_list[z]), end = "")
+                y += 1
             check += 1
 
         except(ValueError):
             return ""
 
-    print("",end="\n")
+    print("", end="\n")
     return y
-
