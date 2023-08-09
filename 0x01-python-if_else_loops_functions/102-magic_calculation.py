e6016f33ad5eeea009beca7dc80ff4f5c78a9351
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-09 05:49:31 +0100

    all

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b, c):
-    if(a < b):
+    if (a < b):
         return c
-    elif(c > b):
+    elif (c > b):
         return a + b
     else:
         return (a * b) - c
