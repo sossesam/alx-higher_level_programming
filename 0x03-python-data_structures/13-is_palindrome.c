2f0f07df421c753784435ca3bcf4d748369575e8
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-12 13:31:28 +0100

    all

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,23 +4,22 @@
 
 int is_palindrome(listint_t **head)
 {
-   listint_t *check = *head;
-    listint_t *temp = *reverse_listint(head);
+listint_t *check = *head;
+listint_t *temp = *reverse_listint(head);
 
-
-
-    while(check != NULL){
-        if (check->n == temp->n)
-        {
-            check = check->next;
-            temp = temp->next;
-        }
-        else{
-            return (-1);
-        }
-        return (1);
-    }
-return (0);
+if(head == NULL){
+return(0);
 }
 
-
+while(check != NULL){
+if (check->n == temp->n)
+{
+check = check->next;
+temp = temp->next;
+}
+else{
+return (0);
+}
+}
+return (1);
+}
