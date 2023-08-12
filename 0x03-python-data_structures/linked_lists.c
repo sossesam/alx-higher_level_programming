09f0e22647f5f9281e1b3e1a65deaf460ba1293d
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-12 13:35:26 +0100

    all

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -72,24 +72,3 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
-
-
-listint_t **reverse_listint(listint_t **head)
-{
-listint_t *p;
-listint_t *n;
-
-p = NULL;
-n = NULL;
-
-while (*head != NULL)
-{
-n = (*head)->next;
-(*head)->next = p;
-p = *head;
-*head = n;
-}
-
-*head = p;
-return (head);
-}
