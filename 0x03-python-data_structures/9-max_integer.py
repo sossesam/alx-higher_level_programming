7d6c4741e6cee31ca64eafd28404f97861729209
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-15 00:11:04 +0100

    all

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0 or my_list == False:
+    if len(my_list) == 0:
         return None
     max = my_list[0]
     for i in my_list:
