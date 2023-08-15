afa05a2905c738ba353d4936ba402feda4762844
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-15 06:44:20 +0100

    all

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     x = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if i % 2 == 0 and i is not 1:
             x.append(True)
         else:
             x.append(False)
