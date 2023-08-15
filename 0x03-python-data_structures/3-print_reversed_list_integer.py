ceb5402d5daedc73b7109c1cb81af95550abc985
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-15 06:31:51 +0100

    all

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list):
-    if hasattr(np.array([1, 2, 3]), "__len__"):
-        i = len(my_list) - 1
-    while i >= 0:
-        print("{:d}".format(my_list[i]))
-        i -= 1
+
+    i = len(my_list) - 1
+    if i >= 0:
+        while i >= 0:
+            print("{:d}".format(my_list[i]))
+            i -= 1
