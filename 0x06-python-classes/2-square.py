2cd002486d27e48af55e591b02fa69ffd30946dd
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-30 11:57:24 +0100

    all

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 """square was initialized"""
 
+
 class Square:
-    """square """
-
-    def __init__(self,size = 0):
-                    if type(size) != int:
-                         raise TypeError("size must be an integer")
-                    elif size < 0:
-                            raise ValueError("size must be >= 0")
-                    else:
-                            self.__size = size
-
-
-
-
-
-
-
-
-
-
+    """ square """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
