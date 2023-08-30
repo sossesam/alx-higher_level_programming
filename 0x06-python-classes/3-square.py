22655e13fe69d22e6baedef58aa90b8f74748ff1
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-30 19:22:03 +0100

    all

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """square was initialized"""
 class Square:
-
+    """we initiallized the square"""
     def __init__(self,size = 0):
             if type(size) != int:
                 raise TypeError("size must be an integer")
@@ -9,7 +9,7 @@ class Square:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
-
+    """ area- calculates the area of the square"""
     def area(self):
             size = self.__size
             return size * size
