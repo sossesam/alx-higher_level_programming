d95a372e63fbdc3d80ddd547a5aa2c4a47cffc6e
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-06 09:46:18 +0100

    all

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""0-square defines a squar"""
+
+
+class Rectangle:
+    """Define width"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
