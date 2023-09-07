bc621ca8108bb209363640b20491e3fb15eda3d0
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-07 08:53:44 +0100

    all

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,50 @@
 
 
 class Rectangle:
-    """Define width"""
+    """instantiate class"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Define width"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        """Define height"""
+        if type(height) != int:
+            raise TypeError("width must be an integer")
+        elif height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(self.__width) != int:
+            raise TypeError("width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+
+
+    @property
+    def height(self, value):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(self.__height) != int:
+            raise TypeError("width must be an integer")
+        elif self.__height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
+
+
