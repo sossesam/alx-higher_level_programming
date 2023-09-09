68a6fdb6faf7c08728b842d2e13e4bf872e7cd3e
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-09 13:29:31 +0100

    all

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -86,9 +86,9 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if isinstance(rect_1, Rectangle) == False:
-            TypeError("rect_1 must be an instance of Rectangle")
-        if isinstance(rect_2, Rectangle) == False:
-            TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif isinstance(rect_2, Rectangle) == False:
+            raise TypeError("rect_2 must be an instance of Rectangle")
         else:
             if rect_1.area() >= rect_2.area():
                 return rect_1
