3d8710de7aa3771c06e29914a82668eb124ff8ff
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-12 07:23:29 +0100

    all

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""takes a list class and returns it in a sorted format"""
+
 
 class MyList(list):
-
+    """the initialisation process"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    """method to sort the list"""
     def print_sorted(self):
 
          print(sorted(self))
