92d41e6c8e1bd4a7a7da81d99757f310b350bc87
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-14 14:55:45 +0100

    all

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,9 +9,10 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-
+    i = 0
     for matrice in matrix :
-
+        if len(matrice) == len(matrix[i]) is False:
+            raise TypeError("Each row of the matrix must have the same size")
         if type(matrice) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         new_list=[]
@@ -21,8 +22,14 @@ def matrix_divided(matrix, div):
                 new_list.append(divided_number)
             else:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+        i+=1
         new_matrix.append(new_list)
 
     return new_matrix
 
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
