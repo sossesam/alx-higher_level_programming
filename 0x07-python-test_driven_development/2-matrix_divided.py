39e94973041633a077a98b4d9164b43a4445ad94
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-14 16:37:24 +0100

    all

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-""" Doc """
+"""
+This module defines function to divide two list int a matrix
+"""
 
 
 def matrix_divided(matrix, div):
-    """ Doc """
-
-    new_matrix = []
-
-    if type(div) != int and type(div) != float:
-        raise TypeError("div must be a number")
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    for dlist in range(len(matrix)):
-        if len(matrix[dlist]) != len(matrix[dlist - 1]):
-            raise TypeError("Each row of the matrix must have the same size")
-
-
-    for matrice in matrix :
-        if type(matrice) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        new_list=[]
-        for x in matrice:
-            if type(x)==int or type(x)==float:
-                divided_number = round((x/div), 2)
-                new_list.append(divided_number)
-            else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-        new_matrix.append(new_list)
-
-    return new_matrix
-
-
+        """
+        Divides list and Raises TypeError
+        """
+        if not isinstance(div, (int, float)):
+                raise TypeError("div must be a number")
+        elif div is 0:
+                raise ZeroDivisionError("division by zero")
+        typeErr = "matrix must be a matrix (list of lists) of integers/floats"
+        sizeErr = "Each row of the matrix must have the same size"
+        new = []
+        if matrix is None or len(matrix) is 0 or len(matrix[0]) is 0:
+                raise TypeError(typeErr)
+        old = len(matrix[0])
+        for count, y in enumerate(matrix):
+                if not isinstance(y, list):
+                        raise TypeError(typeErr)
+                if len(y) != old:
+                        raise TypeError(sizeErr)
+                old = len(y)
+                new.append(y[:])
+                for a, item in enumerate(y):
+                        if not isinstance(item, (int, float)):
+                                raise TypeError(typeErr)
+                        new[count][a] = round(item / div, 2)
+        else:
+                return (new)
