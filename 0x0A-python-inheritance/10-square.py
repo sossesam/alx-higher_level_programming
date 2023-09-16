7febf39bf16a52379f2ec9aa427928cb741112eb
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-16 15:52:07 +0100

    all" git push "

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,8 +4,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
+    """pass"""
     def __init__(self, size):
+        """pass"""
         self.integer_validator("size", size)
         self.__size = size
         Rectangle.__init__(self, self.__size, self.__size)
