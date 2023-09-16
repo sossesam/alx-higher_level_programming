84c8190ec3b9dda8937f115a066b101e40d24dbf
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-16 15:55:37 +0100

    all" git push "

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,3 +10,10 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         Rectangle.__init__(self, self.__size, self.__size)
+
+    def __str__(self):
+        """
+        string representation of the rectangle
+        """
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
+
