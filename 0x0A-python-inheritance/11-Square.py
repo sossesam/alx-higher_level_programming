da133951041f2f947e4e100b1d2c4ad746a4077d
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-16 15:56:18 +0100

    all

--- 0x0A-python-inheritance/11-Square.py
+++ 0x0A-python-inheritance/11-Square.py
@@ -11,4 +11,9 @@ class Square(Rectangle):
         self.__size = size
         Rectangle.__init__(self, self.__size, self.__size)
 
-    
+    def __str__(self):
+        """
+        string representation of the rectangle
+        """
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
+
