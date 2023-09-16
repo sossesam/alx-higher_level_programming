996bd2f91563910fa7deacc19ccd9aa9d2dc87a6
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-16 14:15:08 +0100

    all

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
 """pass"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
 
 
 class Rectangle(BaseGeometry):
