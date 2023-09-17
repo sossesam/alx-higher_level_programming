623123378e1113df4da21001be30e8d04c7476b4
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-17 19:31:11 +0100

    all

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@
 import json
 
 
+
 def save_to_json_file(my_obj, filename):
     """pass"""
     with open(filename, "w") as file:
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""pass"""
+
+import json
+def load_from_json_file(filename):
+    with open(filename, "r") as object:
+        data = json.load(object)
+    return data
+
