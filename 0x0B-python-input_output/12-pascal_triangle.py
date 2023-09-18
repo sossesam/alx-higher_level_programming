569bc2fe96cae550da9578b08ad10b992a5418e0
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-18 13:30:31 +0100

    all

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,22 +17,18 @@ def pascal_triangle(n):
         newlist.append(1)
 
         if array_position >= 2:
-            prev =mainlist[-1]
+            prev = mainlist[-1]
             for y in range(2, array_position):
                 if y <= array_position and array_position > 1:
-                    prev =mainlist[-1]
+                    prev = mainlist[-1]
             for num in range(len(prev) - 1):
                 sum = prev[num] + prev[num + 1]
                 newlist.append(sum)
 
-
             newlist.append(1)
-
-
 
         mainlist.append(newlist)
 
         array_position += 1
-
 
     return mainlist
