86dfd0b5020c7186950a91f8ab98bda16a73e9ee
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-19 22:16:46 +0100

    all

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""pass"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """pass"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        items = self.__dict__
+        new_dict = {}
+
+        if attrs is not None:
+            for title in attrs:
+                if title in items.keys():
+                    new_dict[title] = items[title]
+            return new_dict
+        else:
+            return items
+
+    def reload_from_json(self, json):
+        self.__dict__ = json
