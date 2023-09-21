901bb42f5f7cfd1ae76e5590b31491b9c74b4588
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-21 07:07:08 +0100

    all

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,16 +5,16 @@ with all its methods and attributes
 definition
 """
 
+
 class Base():
     """
     This class has a private attribute __nb_objects = 0
     a class constructor definition to check for id
     """
 
-
     __nb_objects = 0
 
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         """
         Instantiation of class which checks for id
         """
