4165b2ff6ae8da75d0b87bcb03a53b4a4e0f0d60
oyeniran samuel sanmi <sossesam@gmail.com>
2023-09-21 07:03:48 +0100

    all

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""pass"""
-
+"""
+This modules contains a class Base
+with all its methods and attributes
+definition
+"""
 
 class Base():
-    """pass"""
+    """
+    This class has a private attribute __nb_objects = 0
+    a class constructor definition to check for id
+    """
 
 
     __nb_objects = 0
 
     def __init__(self, id = None):
-        """pass"""
-
-
+        """
+        Instantiation of class which checks for id
+        """
         if id is not None:
             self.id = id
         else:
