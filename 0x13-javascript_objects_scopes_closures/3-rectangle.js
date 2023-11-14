133e27697776a4d3783c8b725dc98edc2d6f5139
sossesam <sossesam@gmail.com>
2023-11-14 20:28:01 +0100

    all

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,24 +6,25 @@ class Rectangle {
       if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
-
-        this.print = function () {
-          let x = 0;
-          while (x < this.height) {
-            let y = 0;
-            while (y < this.width) {
-              process.stdout.write('X');
-              y++;
-            }
-            console.log();
-            x++;
-          }
-        };
-      }
     } else {
       return this;
     }
   }
+}
+
+print() {
+  let x = 0;
+  while (x < this.height) {
+    let y = 0;
+    while (y < this.width) {
+      process.stdout.write('X');
+      y++;
+    }
+    console.log();
+    x++;
+  }
+};
+
 }
 
 module.exports = Rectangle;
