c6d400f41d9755675625b7e96ffb6b665760789d
sossesam <sossesam@gmail.com>
2023-11-15 02:01:52 +0100

    all

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,6 @@
+#!/usr/bin/node
 exports.converter = function (base) {
-  function convert (number, base) {
-    let convertedNumber = '';
-    while (number) {
-      const remainder = number % base;
-      convertedNumber = remainder + convertedNumber;
-      number = Math.floor(number / base);
-    }
-    return convertedNumber;
-  }
-
-  return function (number) {
-    return convert(number, base);
+  return function (newbase) {
+    return newbase.toString(base);
   };
 };
