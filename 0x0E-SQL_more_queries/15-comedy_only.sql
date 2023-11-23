b70ac0e403d556ffd4b8422a81c732480865a1a5
sossesam <sossesam@gmail.com>
2023-11-23 15:58:33 +0100

    all

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
-USE hbtn_0d_tvshows;
-SELECT title FROM tv_shows
+
+SELECT tv_shows.title FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = 'Comedy' ORDER BY title ASC;
