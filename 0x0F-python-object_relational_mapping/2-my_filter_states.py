14a77138f9b07cecdf4d798fb240cc15ef362699
sossesam <ssossesam@gmail.com>
2023-12-10 11:27:11 -0800

    all

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,10 +24,9 @@ if __name__ == "__main__":
                    charset="utf8")
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT * FROM states")
+    cur.execute(f"SELECT * FROM states WHERE name='{s}'")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1] == s:
-            print(row)
+        print(row)
     cur.close()
     conn.close()
