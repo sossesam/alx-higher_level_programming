fd09b0da3c9b4ca5c34ffa2c4e072fb41742bc3b
sossesam <ssossesam@gmail.com>
2023-12-12 08:23:17 -0800

    äll

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,11 +7,11 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     the state class creation with sqlalchemy
-
     """
     __tablename__ = "states"
-    id =  Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
