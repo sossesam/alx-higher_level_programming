b5fd1db3d054ba8d413482fb3c8082c373777bbe
sossesam <ssossesam@gmail.com>
2023-12-12 18:17:55 -0800

    äll

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy import asc, desc
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                    sys.argv[1],
+                    sys.argv[2], sys.argv[3]
+                    ),
+                pool_pre_ping=True
+                )
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    result = session.query(State).filter(State.id==1).order_by(asc(State.id))
+
+    for r in result:
+        print(f"{r.id}: {r.name}")
