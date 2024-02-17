d85db9f9e6206feaeb0d932ef1c99868dbcc5559
sossesam <sossesam@gmail.com>
2024-02-17 06:15:30 -0800

    all

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+def find_peak(number):
+    pos = 0
+    maxiim = 0
+    if len(number) == 0:
+        return None
+
+    if len(number) % 2 == 0:
+        max1 = 0
+        while pos < len(number) - 1:
+            
+            if number[pos] >  number[pos + 1]:
+                max1 = number[pos]
+            else:
+                max1 = number[pos + 1]
+            
+            if maxiim < max1:
+                maxiim = max1
+
+            pos = pos + 2
+    else:
+        max1 = 0
+
+        if number[pos] >  number[pos + 1]:
+                max1 = number[pos]
+        pos = pos + 1
+        
+        while pos < len(number) - 1:
+            
+            if number[pos] >  number[pos + 1]:
+                max1 = number[pos]
+            
+            else:
+                max1 = number[pos + 1]
+            
+            if maxiim < max1:
+                maxiim = max1
+
+            pos = pos + 2
+
+
+    return maxiim
+
+
+
+
