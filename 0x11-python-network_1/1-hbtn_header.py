7592388fa3c5e295e1ae7d392fcfadcc5d18b059
sossesam <sossesam@gmail.com>
2024-02-17 19:45:09 +0100

    all

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" This module find the highest number """
-
+"""
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+"""
 import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    x_request_id = response.info()
-    print(x_request_id.get("X-Request-Id"))
-    
-
-
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))
 
     
