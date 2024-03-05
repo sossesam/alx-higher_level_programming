aa311bff33540634ce34e0a568ad04c34a016e7d
sossesam <sossesam@gmail.com>
2024-03-05 14:31:27 -0800

    everything

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,3 @@ with req.urlopen("https://alx-intranet.hbtn.io/status") as response:
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(content.decode('utf-8')))
-
