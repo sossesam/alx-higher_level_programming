782fc922bca4213ced2c735e9df86ad6888db340
sossesam <sossesam@gmail.com>
2024-03-26 21:43:02 -0700

    all

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-// Requiring fs module in which 
-// readFile function is defined.
 const fs = require('fs');
- 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-if(err){ console.log(err)}
-else{
- console.log(data.toString());
-}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
